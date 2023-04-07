bb6a1d04a1be78af5edb516b5d60f924e99f7a69
ZiynatII <zisomurodova@gmail.com>
2023-04-07 21:39:05 +0500

    Main.setConnectionSendMassage created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,29 @@
 package org.example;
 
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new Bot());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        setConnectionSendMassage("Hi:)");
+    }
+
+    public static void setConnectionSendMassage(String s) throws IOException {
+        Config cfg = ConfigFactory.parseFile(new File("application.conf"));
+        URL url = new URL("https://api.telegram.org/bot" + cfg.getString("telegram.bot.token")
+                + "/sendMessage?chat_id=" + cfg.getString("telegram.chatId") + "&text=" + s);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        InputStream is = con.getResponseCode() <= 299 ? con.getInputStream() : con.getErrorStream();
+        String content;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+            content = reader.lines().collect(Collectors.joining("\n"));
         }
+        System.out.println(content);
     }
 }
