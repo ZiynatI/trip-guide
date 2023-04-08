6a720521b4a9eb1d78d51e57ba6a1c6696e8460a
ZiynatII <zisomurodova@gmail.com>
2023-04-08 11:12:57 +0500

    Main.java updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,18 +10,17 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String urlWithoutMessage = getUrlWithoutMessage();
-        sendMessage("Hi)", urlWithoutMessage);
+        String[] tokenChatId = getUrlWithoutMessage();
+        sendMessage("Hi)", tokenChatId[0], tokenChatId[1]);
     }
 
-    public static String getUrlWithoutMessage() {
+    public static String[] getUrlWithoutMessage() {
         Config cfg = ConfigFactory.parseFile(new File("application.conf"));
-        return "https://api.telegram.org/bot" + cfg.getString("telegram.bot.token")
-                + "/sendMessage?chat_id=" + cfg.getString("telegram.chatId") + "&text=";
+        return new String[]{cfg.getString("telegram.bot.token"), cfg.getString("telegram.chatId")};
     }
 
-    public static void sendMessage(String message, String urlWithoutMessage) throws IOException {
-        URL url = new URL(urlWithoutMessage + message);
+    public static void sendMessage(String message, String token, String chatId) throws IOException {
+        URL url = new URL("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId + "&text=" + message);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         InputStream is = con.getResponseCode() <= 299 ? con.getInputStream() : con.getErrorStream();
