58b014ebb7d1a4c00366288587a5bf66bc8c0098
ZiynatII <zisomurodova@gmail.com>
2023-04-08 11:28:07 +0500

    Bot.java created

---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class Bot {
+
+    public void sendMessage(String message, String token, String chatId) throws IOException {
+        URL url = new URL("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId + "&text=" + message);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        InputStream is = con.getResponseCode() <= 299 ? con.getInputStream() : con.getErrorStream();
+        String content;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+            content = reader.lines().collect(Collectors.joining("\n"));
+        }
+        System.out.println(content);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,25 +4,10 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Config cfg = ConfigFactory.parseFile(new File("application.conf"));
-        sendMessage("Hi)", cfg.getString("telegram.bot.token"), cfg.getString("telegram.chatId"));
-    }
-
-    public static void sendMessage(String message, String token, String chatId) throws IOException {
-        URL url = new URL("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId + "&text=" + message);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        InputStream is = con.getResponseCode() <= 299 ? con.getInputStream() : con.getErrorStream();
-        String content;
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-            content = reader.lines().collect(Collectors.joining("\n"));
-        }
-        System.out.println(content);
+        new Bot().sendMessage("Hi)", cfg.getString("telegram.bot.token"), cfg.getString("telegram.chatId"));
     }
 }
