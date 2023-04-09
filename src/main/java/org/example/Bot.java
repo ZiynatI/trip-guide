17ba961ab03a40b74ca5be31c314e52a16e1b00f
ZiynatII <zisomurodova@gmail.com>
2023-04-09 13:21:08 +0500

    Bot updated

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -23,9 +23,8 @@ public class Bot {
     }
 
     public void sendMessage(String title, String message, String chatId) throws IOException {
-        System.out.println(URLEncoder.encode(message, StandardCharsets.UTF_8));
         URL url = new URL("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId + collectParamsToString()
-                + "&text=" + URLEncoder.encode("*" + correctReservedChars(title) + "*" + "\n" + correctReservedChars(message), StandardCharsets.UTF_8));
+                + "&text=" + URLEncoder.encode("*" + escapeReservedChars(title) + "*" + "\n" + escapeReservedChars(message), StandardCharsets.UTF_8));
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         InputStream is = con.getResponseCode() <= 299 ? con.getInputStream() : con.getErrorStream();
@@ -33,10 +32,9 @@ public class Bot {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
             content = reader.lines().collect(Collectors.joining("\n"));
         }
-        System.out.println(content);
     }
 
-    public static String correctReservedChars(String message) {
+    public static String escapeReservedChars(String message) {
         Character[] reserved = new Character[]{'!', '*', '\'', '(', ')', ';', ':', '@', '&', '=', '+', '$', ',', '/', '?', '%', '#', '[', ']' };
         for (char c : reserved) {
             message = message.replace(Character.toString(c), "\\" + c);
