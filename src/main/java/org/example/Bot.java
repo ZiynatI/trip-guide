6957ca921a542b20dab1816d2dc3bdf14d872906
ZiynatII <zisomurodova@gmail.com>
2023-04-11 00:10:17 +0500

    Bot.getCityNum created

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -24,9 +25,33 @@ public class Bot {
         params.put("parse_mode", "MarkdownV2");
     }
 
+    public static void sendRequest() throws MalformedURLException {
+        URL url = new URL("https://e-ticket.railway.uz/api/v1/trains/availability/space/between/stations");
+        String request = "POST /api/v1/trains/availability/space/between/stations HTTP/1.1\n" +
+                "Host: e-ticket.railway.uz\n\n";
+    }
+
+    public int getCityNum() throws IOException {
+        URL url = new URL("https://api.telegram.org/bot" + token + "/getUpdates?offset=-1");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        InputStream is = con.getResponseCode() <= 299 ? con.getInputStream() : con.getErrorStream();
+        String content;
+        Map response;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+            content = reader.lines().collect(Collectors.joining("\n"));
+            response = parse(content);
+            System.out.println(content);
+            if (response.get("ok").equals("false")) {
+                throw new IOException("error_code:" + response.get("error_code") + response.get("description"));
+            }
+        }
+        return Integer.parseInt(content.substring(content.lastIndexOf(':') + 2, content.lastIndexOf('\"')));
+    }
+
     public void sendMessage(String title, String message, String chatId) throws IOException {
         URL url = new URL("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId + collectParamsToString()
-                + "&text=" + URLEncoder.encode("*" + escapeReservedChars(title) + "*" + "\n" + escapeReservedChars(message), StandardCharsets.UTF_8) + "!");
+                + "&text=" + URLEncoder.encode("*" + escapeReservedChars(title) + "*" + "\n" + escapeReservedChars(message), StandardCharsets.UTF_8));
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         InputStream is = con.getResponseCode() <= 299 ? con.getInputStream() : con.getErrorStream();
@@ -35,14 +60,14 @@ public class Bot {
             content = reader.lines().collect(Collectors.joining("\n"));
             Map response = parse(content);
             System.out.println(response);
-            if (response.get("ok").equals("…false")) {
+            if (response.get("ok").equals("false")) {
                 throw new IOException("error_code:" + response.get("error_code") + response.get("description"));
             }
         }
     }
 
     public static String escapeReservedChars(String message) {
-        Character[] reserved = new Character[]{'!', '*', '\'', '(', ')', ';', ':', '@', '&', '=', '+', '$', ',', '/', '?', '%', '#', '[', ']' };
+        Character[] reserved = new Character[]{'!', '*', '\'', '(', ')', ';', ':', '@', '&', '=', '+', '$', ',', '/', '?', '%', '#', '[', ']', '.' };
         for (char c : reserved) {
             message = message.replace(Character.toString(c), "\\" + c);
         }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,49 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Config cfg = ConfigFactory.parseFile(new File("application.conf"));
         Bot bot = new Bot(cfg.getString("telegram.bot.token"));
-        bot.sendMessage("Title! *Part two", "*Hello There!*", cfg.getString("telegram.chatId"));
+//        bot.sendMessage("Title! *Part two", "*Hello There!*", cfg.getString("telegram.chatId"));
+        bot.sendMessage("Select the number of city you are leaving from", citiesToString(), cfg.getString("telegram.chatId"));
+        Thread.sleep(10000);
+        int cityFrom = bot.getCityNum();
+        bot.sendMessage("Select the city you are going to", citiesToString(), cfg.getString("telegram.chatId"));
+        Thread.sleep(10000);
+        int cityTo = bot.getCityNum();
+
+    }
+
+    private static List<String> cities() {
+        List<String> list = new LinkedList<>();
+        list.add("TASHKENT");
+        list.add("SAMARKAND");
+        list.add("BUKHARA");
+        list.add("KHIVA");
+        list.add("URGENCH");
+        list.add("NUKUS");
+        list.add("NAVOI");
+        list.add("ANDIJAN");
+        list.add("KARSHI");
+        list.add("DJIZAKH");
+        list.add("TERMEZ");
+        list.add("GULISTAN");
+        list.add("KOKAND");
+        list.add("MARGILAN");
+        list.add("PAP");
+        list.add("NAMANGAN");
+        return list;
+    }
+
+    private static String citiesToString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= cities().size(); i++) {
+            sb.append(i).append(". ").append(cities().get(i - 1)).append("\n");
+        }
+        return sb.toString();
     }
 }
