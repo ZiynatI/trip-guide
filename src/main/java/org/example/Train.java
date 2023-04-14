fd7add4bf619ce3c87311ad6fb86fa5e737adc4f
ZiynatII <zisomurodova@gmail.com>
2023-04-14 10:36:54 +0500

    Train.java created

---
+++ src/main/java/org/example/Train.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Train {
+    private String length;
+    private String type;
+    private String number;
+    private String brand;
+    private Route route;
+}
+
+class Route {
+    private String[] station;
+}
