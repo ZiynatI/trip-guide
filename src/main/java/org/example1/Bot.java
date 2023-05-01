ba941225b77e9e11625529fcb804eb5fd712b9c1
ZiynatII <zisomurodova@gmail.com>
2023-05-01 23:09:27 +0500

    example package created

--- src/main/java/org/example1/Bot.java
+++ src/main/java/org/example1/Bot.java
@@ -1,39 +1,110 @@
 package org.example1;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 public class Bot extends TelegramLongPollingBot {
-    public static void main(String[] args) throws TelegramApiException {
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(new Bot("5862917032:AAGjoeDEgWFkH0ER5gGvZ9LprXl4t86SfEU"));
-    }
 
-    private String token;
+    private final String botToken;
+    private final String botUserName;
+    private LinkedList<String> messages;
 
-    Bot(String token) {
-        this.token = token;
+
+    private List<List<InlineKeyboardButton>> stationButtons;
+
+    public Bot(String botToken, String botUserName) {
+        this.botToken = botToken;
+        this.botUserName = botUserName;
+        messages = new LinkedList<>();
     }
 
     @Override
     public String getBotUsername() {
-        return "ZisTripGuideBot";
+        return this.botUserName;
     }
 
     @Override
     public String getBotToken() {
-        return token;
+        return this.botToken;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        var msg = update.getMessage();
-        var user = msg.getFrom();
-
-        System.out.println(user.getFirstName() + " wrote " + msg.getText());
+        if (update.hasCallbackQuery()) {
+            handleCallback(update.getCallbackQuery());
+        }
+        if (update.hasMessage()) {
+            try {
+                handleMassage(update.getMessage());
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+//        var msg = update.getMessage();
+//        var user = msg.getFrom();
+//        messages.add(msg.getText());
+//        System.out.println(user.getFirstName() + " wrote :\"" + msg.getText() + '"');
     }
 
+    private void handleCallback(CallbackQuery callbackQuery) {
+        Message message = callbackQuery.getMessage();
+        String[] param = callbackQuery.getData().split(":");
+        String action = param[0];
+        String newTemp = param[1];
+        switch (action) {
+            case "departure":
+            case "arrival":
+        }
+    }
+
+    private void handleMassage(Message message) throws TelegramApiException {
+        if (message.hasText() && message.hasEntities()) {
+            Optional<MessageEntity> commandEntity = message.getEntities().stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
+            if (commandEntity.isPresent()) {
+                message.getText().substring(commandEntity.get().getOffset(), commandEntity.get().getLength());
+                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                markupInline.setKeyboard(stationButtons);
+                execute(SendMessage.builder().text("Choose station to leave from").chatId(message.getChatId()).replyMarkup(InlineKeyboardMarkup.builder().keyboard(stationButtons).build()).build());
+
+            }
+        }
+    }
+
+    public void sendText(String chatId, String message) {
+        SendMessage sm = SendMessage.builder()
+                .chatId(chatId)
+                .text(message).build();
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getFirstMessage() {
+        return messages.remove();
+    }
+
+    public void clearMessages() {
+        messages.clear();
+    }
+
+    public List<List<InlineKeyboardButton>> getStationButtons() {
+        return stationButtons;
+    }
+
+    public void setStationButtons(List<List<InlineKeyboardButton>> stationButtons) {
+        this.stationButtons = stationButtons;
+    }
 }
